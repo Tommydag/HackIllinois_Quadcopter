c363966345f964546842cca1d9d9a0a424e6c9f4
localmgr <igloo@ubuntu.ubuntu-domain>
2014-04-12 10:44:50 -0400

    The key_control.js file has been updated to include WASD controls.

--- key_control.js
+++ key_control.js
@@ -1,5 +1,6 @@
 var ardrone = require('ar-drone');
 var client = ardrone.createClient();
+var control = ardrone.createUdpControl();
 
 var stdin = process.stdin;
 
@@ -8,35 +9,38 @@ keypress(process.stdin);
 require('tty').setRawMode(true);
 
 process.stdin.on('keypress',function(chunk,key) {
-	if(key == '5') client.stop();
-	else if(chunk == '4') {
+	if(key == '5' || chunk == 'x') client.stop();
+	else if(chunk == '4' || chunk == 'a') {
 		client.stop();
 		client.left(0.1);
-	} else if(chunk == '6') {
+	} else if(chunk == '6' || chunk == 'd') {
 		client.stop();
 		client.right(0.1);
-	} else if(chunk == '8') {
+	} else if(chunk == '8' || chunk == 'w') {
 		client.stop();
 		client.front(0.1);
-	} else if(chunk == '2') {
+	} else if(chunk == '2' || chunk == 's') {
 		client.stop();
 		client.back(0.1);
-	} else if(chunk == '+') {
+	} else if(chunk == '+' || chunk == 'j') {
 		client.stop();
-		client.up(0.1);
-	} else if(chunk == '-') {
+		client.up(1);
+	} else if(chunk == '-' || chunk == 'k') {
 		client.stop();
-		client.down(0.1);
-	} else if(chunk == '0') {
+		client.down(0.2);
+	} else if(chunk == '0' || chunk == 'i') {
 		client.land();
-	} else if(chunk == '.') {
+	} else if(chunk == '.' || chunk == '.') {
 		client.takeoff();
 	} else if(chunk == 'q') {
 		client.stop();
 		process.kill();
 	} else if(chunk == 'f') {
 		client.stop();
-		client.animate('flipAhead',1000);
+		client.animate('flipAhead',250);
+	}
+	else if(chunk == 'y') {
+		client.land();
+		control.ref({fly: true, emergency: true});
 	}
 });
-
