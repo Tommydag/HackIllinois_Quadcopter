e81ef533df18bac2ddb566b83edf95c91771b338
mulctv <mulctv@ubuntu.(none)>
2014-04-12 16:02:57 -0400

    PID for pitch

---
+++ PIDpitchtest.js
@@ -0,0 +1,81 @@
+//PID
+//Pitch
+var P_pitch = 1;
+var I_pitch = 0;
+var D_pitch = 0;
+//Roll
+var P_roll = 1;
+var I_roll = 1;
+var D_roll = 1;
+
+//set desired pitch and roll values
+var MV_pitch = 0;
+var MV_roll = 0;
+
+//stores all pitch values
+var total_pitch_error = 0;
+
+//Create error variables: pitch and roll
+function Error_PID(MV,PV){
+	return MV-PV;}
+
+function Pitch_PID(MV,pitch_c,count){
+	//P
+	//calculate error from set pitch to current pitch
+	var error_pitch_c=Error_PID(MV_pitch,pitch_c);
+
+	//I
+	//Sum of previous pitches
+	total_pitch_error=total_pitch_error+error_pitch_c;
+
+	//D
+	//Change from previous pitch
+	if(count==0){var error_pitch_p = error_pitch_c;}
+	else{
+	var delta_pitch_error=error_pitch_c-error_pitch_p;}
+
+	//Compute MV
+	MV_pitch_out=P_pitch*(error_pitch_c)+I_pitch*(total_pitch_error)+D_pitch*(delta_pitch_error);
+
+	//sets current pitch_n to pitch_(n-1), current pitch to previous pitch
+	error_pitch_p = error_pitch_c;
+
+	//gives drone new pitch value
+	client.front(-MV_pitch_out);
+}
+	
+
+//connect to drone
+var ardrone = require('ar-drone');
+var client = ardrone.createClient();
+//Hovermode
+var count = 0;
+//begin hovermode
+client.on('navdata', function(datalog){
+	//Get values for current pitch, p
+	var pitch_c = datalog.demo.rotation.pitch;
+
+	//P
+	//calculate error from set pitch to current pitch
+	var error_pitch_c=Error_PID(MV_pitch,pitch_c);
+
+	//I
+	//Sum of previous pitches
+	total_pitch_error=total_pitch_error+error_pitch_c;
+
+	//D
+	//Change from previous pitch
+	if(count==0){var error_pitch_p = error_pitch_c;}
+	else{
+	var delta_pitch_error=error_pitch_c-error_pitch_p;}
+
+	//Compute MV
+	MV_pitch_out=P_pitch*(error_pitch_c)+I_pitch*(total_pitch_error)+D_pitch*(delta_pitch_error);
+
+	//sets current pitch_n to pitch_(n-1), current pitch to previous pitch
+	error_pitch_p = error_pitch_c;
+
+	//gives drone new pitch value
+	client.front(-MV_pitch_out);
+	count++
+});
